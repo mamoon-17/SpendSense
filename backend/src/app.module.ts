633dfb475e5ef1b564971bf057f379a5ee2bef8f
mamoon-17 <mamoonmasood17@gmail.com>
2025-09-26 00:09:31 +0500

    commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST', { infer: true }),
+        port: configService.get<number>('DATABASE_PORT', { infer: true }),
+        username: configService.get<string>('DATABASE_USERNAME', {
+          infer: true,
+        }),
+        password: configService.get<string>('DATABASE_PASSWORD', {
+          infer: true,
+        }),
+        database: configService.get<string>('DATABASE_NAME', {
+          infer: true,
+        }),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
